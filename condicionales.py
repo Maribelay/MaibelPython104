4438246b6ca2e9b990d96b1229cbd3fd1bb95937
Maribelay <maribelaguilary.com>
2025-05-29 23:38:22 -0500

    version 2

---
+++ condicionales.py
@@ -0,0 +1,8 @@
+print("Ejemplos de condicionales")
+valores = [4, 6, 8, 10]
+if all(x % 2 == 0 for x in valores):
+    print ("Todos son pares")
+elif any (x % 2 == 0 for x in valores): 
+    print ("Algunos son pares")
+else:
+    print("Ninguno es par")
