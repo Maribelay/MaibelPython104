f3f2106556070daa108752434d12133d1f394fa6
Maribelay <maribelaguilary.com>
2025-06-06 16:02:13 -0500

    version5

--- Algoritmo.py
+++ Algoritmo.py
@@ -1,1 +1,7 @@
 print("hola como estan")
+def mcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+print(mcd(48, 18)) 
--- Estructura datos2.py
+++ Estructura datos2.py
@@ -1,1 +1,4 @@
 print("modificación")
+coordenadas = (10.5, 20.3)
+x, y = coordenadas
+print(x, y) 
