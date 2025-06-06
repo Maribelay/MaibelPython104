23eabcdc9a28472d0d7ee0a34a893e1c4b4fa4d2
Maribelay <maribelaguilary.com>
2025-06-06 15:15:10 -0500

    version2

--- bucles.py
+++ bucles.py
@@ -1,1 +1,6 @@
-print("Ejemplo de condicionales")
+print("Ejemplo de bucles")
+nombres = ["Ana", "Luis", "Carlos"]
+edades = [23, 34, 19]
+
+for nombre, edad in zip(nombres, edades):
+    print(f"{nombre} tiene {edad} años.")
